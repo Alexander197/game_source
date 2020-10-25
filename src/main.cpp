7a376f51f98f7b065eaf9198ed72e39ac3df6fea
Alexander Skvortsov <asu197791@gmail.com>
2020-10-26 00:19:15 +0200

    added shader renderer class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+#include "Renderer/ShaderProgram.h"
 
 #include <iostream>
 
@@ -83,21 +84,15 @@ int main(void)
 	
 	glClearColor(1,1,1,1);
 
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, nullptr);
-    glCompileShader(vs);
+    std::string vertexShader(vertex_shader);
+    std::string fragmentShader(fragment_shader);
 
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragment_shader, nullptr);
-    glCompileShader(fs);
-
-    GLuint shader_program = glCreateProgram();
-    glAttachShader(shader_program, vs);
-    glAttachShader(shader_program, fs);
-    glLinkProgram(shader_program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
+    if (!shaderProgram.isCompiled())
+    {
+        std::cerr << "Can't create shader program!" << std::endl;
+        return -1;
+    }
 
     GLuint points_vbo = 0;
     glGenBuffers(1, &points_vbo);
@@ -128,7 +123,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program);
+        shaderProgram.use();
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
