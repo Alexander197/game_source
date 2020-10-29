ebf66ec6becfd1296a9728064d23879399163884
Alexander Skvortsov <asu197791@gmail.com>
2020-10-29 14:26:04 +0200

    added glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/vec2.hpp>
 
 #include <iostream>
 
 #include "Renderer/ShaderProgram.h"
 #include "Resources/ResourceManager.h"
 #include "Renderer/Texture2D.h"
+
 
 GLfloat points[] = {
     0.0f,  0.5f, 0.0f,
@@ -23,14 +25,13 @@ GLfloat texCoords[] = {
     0.0f, 0.0f
 };
 
-int g_windowSizeX = 640;
-int g_windowSizeY = 480;
+glm::ivec2 g_windowSize(680, 480);
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) 
 {
-    g_windowSizeX = width;  
-    g_windowSizeY = height;
-    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+    g_windowSize.x = width;  
+    g_windowSize.y = height;
+    glViewport(0, 0, width, height);
 }
 
 void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
@@ -52,7 +53,7 @@ int main(int argc, char** argv)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Hello World", nullptr, nullptr);
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSize.x, g_windowSize.y, "Hello World", nullptr, nullptr);
     if (!pWindow)
     {
         std::cout<<"gldwCreateWindow failed!"<<std::endl;
